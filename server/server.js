18d72bb6ef0f1e4a559968949eb0da520504eb48
Ethan Stenis <ethanstenis@gmail.com>
2018-06-06 14:38:00 -0600

    Lesson 2 Code that matches GitHub answer file

--- server/server.js
+++ server/server.js
@@ -1,21 +1,20 @@
 // TODO: make this work.
 // if yuo go to localhost:3000 the app
 // there is expected crud to be working here
-var express = require('express');
-var bodyParser = require('body-parser');
+import express, { static } from 'express';
+import { urlencoded, json } from 'body-parser';
 var app = express();
 var _ = require('lodash');
-
 // express.static will serve everything
 // with in client as a static resource
 // also, it will server the index.html on the
 // root of that directory on a GET to '/'
-app.use(express.static('client'));
+app.use(static('client'));
 
 // body parser makes it possible to post JSON to the server
-// we can accss data we post on as req.body
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
+// we can access data we post on as req.body
+app.use(urlencoded({extended: true}));
+app.use(json());
 
 
 var lions = [];
@@ -23,5 +22,50 @@ var id = 0;
 
 // TODO: make the REST routes to perform CRUD on lions
 
-app.listen(3000);
-console.log('on port 3000');
+app.get('/lions', function(_req, res) {
+    res.json(lions);
+});
+
+app.get('/lions/:id', function(req, res) {
+    var lion = _.find(lions, {id: req.params.id});
+    res.json(lion || {});
+});
+
+app.post('/lions', function(req,res) {
+    var lion = req.body;
+    id++;
+    lion.id = id + '';
+
+    lions.push(lion);
+
+    res.json(lion);
+});
+
+app.put('/lions/:id', function(req, res) {
+    var update = req.body;
+    if (update.id) {
+        delete update.id
+    }
+
+    var lion = _.findIndex(lions, {id: req.params.id});
+    if (!lions[lion]) {
+        res.send();
+    } else {
+        var updatedLion = _.assign(lions[lion], update)
+        res.json(updatedLion);
+    }
+});
+
+app.delete('/lions/:id', function(req, res) {
+    var lions = _.findIndex(lions, {id: req.params.id});
+    if (!lions[lion]) {
+    res.send();
+    } else {
+        var deletedLion = lions[lion];
+        lions.splice(lion, 1);
+        res.json(deletedLion);
+    }
+});
+
+app.listen(8080);
+console.log('on port 8080');
